e4074fd6ce6f00c6f86661709aef506b184ccb1c
sorojasp <stivenorlandorojaspulido@gmail.com>
2019-02-13 11:53:36 -0500

    function to identify holidays in Colombia between to dates

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 let hollidaysChibchombia = require("colombia-holidays")
 let dateFormat = require('date-format');/*
 the files about this package is in : https://www.npmjs.com/package/dateformat
+another package to handle date:
+https://www.npmjs.com/package/date-and-time
 */ 
+
 let DateDiff = require('date-diff');/* 
 the files about this package is in : https://www.npmjs.com/package/date-diff
 */
@@ -16,49 +19,77 @@ resultados=hollidaysChibchombia.getColombiaHolidaysByYear(2019)[6].holiday;
 
 console.log(resultados.split("-"))
 
+let objectDateToNumber=(date)=>{
+    let year=parseInt(date.year)*10000;
+    let month=parseInt(date.month)*100;
+    let day=parseInt(date.day);
+    return (year+month+day)
+}
+
 
 
 let getHolidays=(dateStart, dateFinish)=>{
-   return new Promise ((resolve,reject)=>{
+    return new Promise((resolve,reject)=>{
+   
         let Allhollydays=[];
-        let yearFinish=dateFinish.year;
-        let monthFinish=dateFinish.month;
-        let dayFinish=dateFinish.day;
-        let year=dateStart.year; month=dateStart.month; day=dateStart.month;
+        let yearFinish=parseInt(dateFinish.year);
+        let yearStart=parseInt(dateStart.year);
+        let year=yearStart;
+        let numberDateStart=objectDateToNumber(dateStart);
+        let numberDateFinish=objectDateToNumber(dateFinish);
+       
         
-        while (year<yearFinish){
+        while (year<=yearFinish){
             let holiDaysYear=hollidaysChibchombia.getColombiaHolidaysByYear(year);
             let i=0;
-            while(month<)
+            while(i<holiDaysYear.length){
+                let dateToTest=(holiDaysYear[i].holiday).split("-");
+                let yearToTest=parseInt(dateToTest[0])*10000;
+                let monthToTest=parseInt(dateToTest[1])*100;
+                let dayToTest=parseInt(dateToTest[2]);
+                let numberDateToTest=yearToTest+  monthToTest+dayToTest;
+                if((numberDateToTest>=numberDateStart)&&(numberDateToTest<=numberDateFinish)){
+                    Allhollydays.push(numberDateToTest)
+                }
+          
+                i++
+            }
             
             
             
-            
-            year++
-
-
-
+            year++;
         }
         
-
-
-       
-
         if(Allhollydays==[]){
             reject("the month choosen for you not was hollidays");
         }else{
             resolve (Allhollydays);
 
         }
-        
-    })
+    }) 
 }
 
-/*
-let hollydayOfMOnth=getHolidays().then(result=>{
-console.log(result)
+let dateStart={
+    year:1992,
+    month:01,
+    day:12
+}//1992-1-12
+
+let dateFinish={
+    year:1996,
+    month:05,
+    day:12
+}//1996-1-12
+
+let hollydays=getHolidays(dateStart,dateFinish).then(result=>{
+console.log(result+ " - resultado final de la función implementada")
 }).catch(err=>{
     console.log(err)
 })
 
-*/
+
+
+
+
+
+
